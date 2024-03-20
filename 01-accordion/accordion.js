e2a9a6f876d2a99a1859c89e83a1768034a99390
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-03-20 23:02:32 +0200

    Update accordion.js

--- 01-accordion/accordion.js
+++ 01-accordion/accordion.js
@@ -1,12 +1,12 @@
-"strict";
+'strict';
 
-const accordionBtns = document.querySelectorAll(".accordion-btn");
-const accordionItems = document.querySelectorAll(".item");
+const accordionBtns = document.querySelectorAll('.accordion-btn');
+const accordionItems = document.querySelectorAll('.item');
 
 for (let i = 0; i < accordionBtns.length; i++) {
-  accordionBtns[i].addEventListener("click", function () {
-    if (!accordionItems[i].classList.contains("open"))
-      accordionItems[i].classList.add("open");
-    else accordionItems[i].classList.remove("open");
+  accordionBtns[i].addEventListener('click', function () {
+    if (!accordionItems[i].classList.contains('open'))
+      accordionItems[i].classList.add('open');
+    else accordionItems[i].classList.remove('open');
   });
 }
