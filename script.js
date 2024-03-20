f3c0137175f2e7cd470679279dc417ec5de8545d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-03-20 23:02:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const previewOpenBtn = document.querySelector('.preview-open-btn');
+const previewCloseBtn = document.querySelector('.preview-close-btn');
+
+previewOpenBtn.addEventListener('click', function () {});
