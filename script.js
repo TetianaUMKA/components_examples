b8998f4c7f27aedb7ddcd6b4e8d219d82efc3c55
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-03-21 21:17:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,15 @@
 
 const previewOpenBtn = document.querySelector('.preview-open-btn');
 const previewCloseBtn = document.querySelector('.preview-close-btn');
+const previewModal = document.querySelector('.preview-modal');
+const overlay = document.querySelector('.overlay');
 
-previewOpenBtn.addEventListener('click', function () {});
+previewOpenBtn.addEventListener('click', function () {
+  previewModal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+previewCloseBtn.addEventListener('click', function () {
+  previewModal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
