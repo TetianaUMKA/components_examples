d510ca608fe9fd8232b54d5a685f1637ecbb15fc
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-03-25 21:26:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 'use strict';
-
-const previewOpenBtn = document.querySelector('.preview-open-btn');
+const previewOpenBtns = document.querySelectorAll('.preview-open-btn');
 const previewCloseBtn = document.querySelector('.preview-close-btn');
 const previewModal = document.querySelector('.preview-modal');
 const overlay = document.querySelector('.overlay');
+const previewImg = document.querySelector('.preview-img');
 
-previewOpenBtn.addEventListener('click', function () {
-  previewModal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-});
+// const openModal = function () {
+//   previewModal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-previewCloseBtn.addEventListener('click', function () {
+const closeModal = function () {
   previewModal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < previewOpenBtns.length; i++) {
+  previewOpenBtns[i].addEventListener('click', function () {
+    previewImg.src = `/images/component-${i + 1}.jpg`;
+    previewModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+previewCloseBtn.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !previewModal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
+
+overlay.addEventListener('click', closeModal);
