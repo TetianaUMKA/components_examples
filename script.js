72b6e4ec51e8f79a6d665b1ba619455016b1fedd
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-04-04 13:07:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,43 @@ const previewModal = document.querySelector('.preview-modal');
 const overlay = document.querySelector('.overlay');
 const previewImg = document.querySelector('.preview-img');
 
-// const openModal = function () {
-//   previewModal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
 const closeModal = function () {
   previewModal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < previewOpenBtns.length; i++) {
-  previewOpenBtns[i].addEventListener('click', function () {
+// Opening the modal with different content
+
+// way #1
+
+// for (let i = 0; i < previewOpenBtns.length; i++) {
+//   previewOpenBtns[i].addEventListener('click', function () {
+//     previewImg.srcset = `images/component-${i + 1}.jpg 1x, images/component-${
+//       i + 1
+//     }-2x.jpg 2x`;
+//     previewImg.src = `images/component-${i + 1}.jpg`;
+//     previewModal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+// }
+
+// way #2
+
+// for (let [i, previewOpenBtn] of previewOpenBtns.entries()) {
+//   previewOpenBtn.addEventListener('click', function () {
+//     previewImg.srcset = `images/component-${i + 1}.jpg 1x, images/component-${
+//       i + 1
+//     }-2x.jpg 2x`;
+//     previewImg.src = `images/component-${i + 1}.jpg`;
+//     previewModal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+// }
+
+// way #3
+
+previewOpenBtns.forEach(function (previewOpenBtn, i) {
+  previewOpenBtn.addEventListener('click', function () {
     previewImg.srcset = `images/component-${i + 1}.jpg 1x, images/component-${
       i + 1
     }-2x.jpg 2x`;
@@ -24,7 +49,7 @@ for (let i = 0; i < previewOpenBtns.length; i++) {
     previewModal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
-}
+});
 
 previewCloseBtn.addEventListener('click', closeModal);
 
